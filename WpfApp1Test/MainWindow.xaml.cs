dd10aafdc281c2c89aa9b66b41bbe71fe487d561
DaniloErazo <erazodanilo1218@gmail.com>
2022-01-30 13:49:57 -0500

    Started to work around implementation for getting statisitcs for pie chart

--- WpfApp1Test/MainWindow.xaml.cs
+++ WpfApp1Test/MainWindow.xaml.cs
@@ -18,59 +18,89 @@ using System.Collections;
 
 namespace WpfApp1Test
 {
-		/// <summary>
-		/// Lógica de interacción para MainWindow.xaml
-		/// </summary>
-		public partial class MainWindow : Window
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		List<DataInfo> municipalities;
+		public MainWindow()
 		{
-				public MainWindow()
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+
+			bool? isContent = openFileDialog.ShowDialog();
+
+			if (isContent == true)
+			{
+				string fileName = openFileDialog.FileName;
+				municipalities = new List<DataInfo>();
+
+				using (StreamReader sr = new StreamReader(openFileDialog.FileName))
 				{
-						InitializeComponent();
+					sr.ReadLine();
+					while (!sr.EndOfStream)
+					{
+						string x = sr.ReadLine();
+						string[] xInfo = x.Split(',');
+						try
+						{
+							DataInfo oneData = new DataInfo(xInfo[0], xInfo[1], xInfo[2], xInfo[3], xInfo[4]);
+							municipalities.Add(oneData);
+							MuniDataGrid.Items.Add(oneData);
+						}
+						catch (Exception ex)
+						{
+						}
+
+					}
 				}
 
-				private void Button_Click(object sender, RoutedEventArgs e)
-				{
-						OpenFileDialog openFileDialog = new OpenFileDialog();
 
-						bool? isContent = openFileDialog.ShowDialog();
 
-						if (isContent == true){
-								string fileName = openFileDialog.FileName;
+				//Code for filtering according to the initial of the department, for 
+				//full implementation, pass the initial as the argument in the predicate of FindAll
 
-								List<DataInfo> datasX = new List<DataInfo>();
+				DataInfo[] arrayInfo = municipalities.ToArray();
 
-								using (StreamReader sr = new StreamReader(openFileDialog.FileName)) {
-									sr.ReadLine();
-									while (!sr.EndOfStream)
-									{
-										string x = sr.ReadLine();
-										string[] xInfo = x.Split(',');
-									try
-									{
-										DataInfo oneData = new DataInfo(xInfo[0], xInfo[1], xInfo[2], xInfo[3], xInfo[4]);
-										datasX.Add(oneData);
-                    MuniDataGrid.Items.Add(oneData);
-									}
-									catch (Exception ex) { 
-									}
-										
-									}
-								}
-
-								dataGrid.ItemsSource = datasX;
-
-								//Code for filtering according to the initial of the department, for 
-								//full implementation, pass the initial as the argument in the predicate of FindAll
-
-								DataInfo[] arrayInfo = datasX.ToArray();
-
-								DataInfo[] filtered = Array.FindAll(arrayInfo, x => x.IsInitial("V"));
-
+				DataInfo[] filtered = Array.FindAll(arrayInfo, x => x.IsInitial("V"));
 
 
 			}
-				}
-
-				
 		}
+
+		public int[] GetStatistics()
+		{
+			int isle = 0, municipality = 0, nonNunicipality = 0;
+			for (int i = 0; i < MuniDataGrid.Items.Count; i++)
+			{
+				string type = municipalities[i].typeX;
+				if (type == "Isla")
+				{
+					isle++;
+				}
+				else if (type == "Municipio")
+				{
+					municipality++;
+				}
+				else
+				{
+					nonNunicipality++;
+				}
+			}
+
+			int[] results = new int[4];
+			results[0] = municipalities.Count;
+			results[1] = municipality;
+			results[2] = isle;
+			results[3] = nonNunicipality;
+
+			return results;
+		}
+
+	}
 }
