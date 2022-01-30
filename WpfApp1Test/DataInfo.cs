d8fe842db155ac2039a0360bca56624468b326ac
JuanFeBlancoT <juanfeblancot@gmail.com>
2022-01-30 02:15:58 -0500

    Initial commit with file dialog and storing all data on list. No table implemented yet

---
+++ WpfApp1Test/DataInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1Test
+{
+		internal class DataInfo
+		{
+				public string depCode { get; set; }
+				public string munCode { get; set; }
+				public string depName { get; set; }
+				public string munName { get; set; }
+				public string typeX { get; set; }
+
+				public DataInfo(string a, string b, string c, string d, string e) {
+						this.depCode = a;
+						this.munCode = b;
+						this.depName = c;
+						this.munName = d;
+						this.typeX = e; 
+				}
+		}
+}
---
+++ WpfApp1Test/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Collections;
+
+namespace WpfApp1Test
+{
+		/// <summary>
+		/// Lógica de interacción para MainWindow.xaml
+		/// </summary>
+		public partial class MainWindow : Window
+		{
+				public MainWindow()
+				{
+						InitializeComponent();
+				}
+
+				private void Button_Click(object sender, RoutedEventArgs e)
+				{
+						OpenFileDialog openFileDialog = new OpenFileDialog();
+
+						bool? isContent = openFileDialog.ShowDialog();
+
+						if (isContent == true){
+								string fileName = openFileDialog.FileName;
+
+								ArrayList datasX = new ArrayList();
+
+								DataGrid dataGrid = new DataGrid();
+								//dataGrid.Columns.Add(new DataGridTextColumn());
+
+								using (StreamReader sr = new StreamReader(openFileDialog.FileName)) {
+										while (!sr.EndOfStream) {
+												string x = sr.ReadLine();
+												string[] xInfo = x.Split(',');
+												DataInfo oneData = new DataInfo(xInfo[0], xInfo[1], xInfo[2], xInfo[3], xInfo[4]);
+												datasX.Add(oneData);
+										}
+								}
+
+							
+						}
+				}
+		}
+}
